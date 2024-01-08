744c7d97b5cbbfc724051a1f264934bbd961d5e2
Amit Dhingra <amitdhingra180@gmail.com>
2024-01-09 02:05:03 +0530

    final

--- src/pages/Diabetes.js
+++ src/pages/Diabetes.js
@@ -1,34 +1,45 @@
-import React from 'react';
-import PatientForm from '../components/PatientForm';
+import React, { useEffect } from 'react';
 
-const Diabetes = () => {
-    const handleFormSubmit = async (formData) => {
-        try {
-          const response = await fetch('http://127.0.0.1:5000/api/diabetes', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formData),
-          });
-    
-          if (response.ok) {
-            const result = await response.json();
-            console.log(result);
-          } else {
-            console.error('Failed to send patient information');
-          }
-        } catch (error) {
-          console.error('Error:', error);
-        }
-      };
-    
-      return (
-        <div>
-          <h1>Patient Information Form</h1>
-          <PatientForm onSubmit={handleFormSubmit} />
-        </div>
-      );
-}
+const SimpleComponent = () => {
 
-export default Diabetes
+  const someStuff = {
+    a:70,
+    b:50,
+    c:50,
+    d:50,
+    e:50,
+    f:50,
+    g:50,
+  }
+
+  console.log(someStuff)
+
+  useEffect(() => {
+    const sendData = async () => {
+      const response = await fetch('http://127.0.0.1:5000/api/diabetes', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(Object.values(someStuff)),
+      });
+
+      const result = await response.json();
+      console.log(result)
+      
+    };
+
+    sendData();
+  }, []);
+
+  
+
+  return (
+    <div>
+      <h1>Simple React Component</h1>
+      <p>This component sends an array of two integers to the Flask backend.</p>
+    </div>
+  );
+};
+
+export default SimpleComponent;
