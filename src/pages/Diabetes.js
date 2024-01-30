c6825181b170fdf53b413e6a0da56a04e36e91ca
Amit Dhingra <amitdhingra180@gmail.com>
2024-01-30 19:59:38 +0530

    final

--- src/pages/Diabetes.js
+++ src/pages/Diabetes.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import InputDiabetes from '../components/InputDiabetes';
 
 
-const SimpleComponent = () => {
+const Diabetes = () => {
 
   const [diabetes_data, setDiabetes_data] = useState({
     Pregnancies:NaN,
@@ -26,7 +26,7 @@ const SimpleComponent = () => {
   }
 
   const handleSubmit = async () => {
-    const response = await fetch('http://127.0.0.1:5000/api/diabetes', {
+    const response = await fetch('https://amitdhingra.pythonanywhere.com/api/diabetes', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -42,51 +42,49 @@ const SimpleComponent = () => {
   
 
   return (
-    <div className="bg-cover bg-center min-h-screen" style={{ backgroundImage: 'url(/diabetes.jpg)' }}>
+    <div>
+   
+    <div className="bg-cover bg-center min-h-screen " style={{ backgroundImage: 'url(/diabetes_bg.png)' }}>
 
-    <div className="absolute inset-0 bg-black opacity-50"></div>
+   <div className='w-full h-full px-4 pr-8 pt-8 sm_md:w-[650px] sm_md:h-[450px] sm_md:absolute sm_md:left-1/2 sm_md:top-1/2 sm_md:transform sm_md:-translate-x-1/2 sm_md:-translate-y-1/2'>
 
-    <div className='w-full min-h-screen relative'>
-      <div className='absolute left-1/2 top-1/2  bg-white px-8 py-4 transform -translate-x-1/2 -translate-y-1/2 flex flex-col rounded-md' >
+    <div className='text-4xl text-white pb-4'>
+      Diabetes
+    </div>
 
-        <div className=' text-4xl text-center pb-2 sm_md:text-left'>
-          Diabetes Prediction
-        </div>
+    <div className='sm_md:flex sm_md:space-x-4'>
+      <InputDiabetes label='Pregnancies' attribute='Pregnancies' onChange={handleChange} />
+      <InputDiabetes label='Glucose' attribute='Glucose' onChange={handleChange} />
+      <InputDiabetes label='BloodPressure' attribute='BloodPressure' onChange={handleChange} />
+    </div>
 
-        <div className='sm_md:flex sm_md:space-x-4'>
-          <InputDiabetes label='Pregnancies' attribute='Pregnancies' onChange={handleChange} />
-          <InputDiabetes label='Glucose' attribute='Glucose' onChange={handleChange} />
-          <InputDiabetes label='BloodPressure' attribute='BloodPressure' onChange={handleChange} />
-        </div>
+    <div className='sm_md:flex sm_md:space-x-4'>
+      <InputDiabetes label='SkinThickness' attribute='SkinThickness' onChange={handleChange} />
+      <InputDiabetes label='Insulin' attribute='Insulin' onChange={handleChange} />
+      <InputDiabetes label='BMI' attribute='BMI' onChange={handleChange} />
+    </div>
 
-        <div className='sm_md:flex sm_md:space-x-4'>
-          <InputDiabetes label='SkinThickness' attribute='SkinThickness' onChange={handleChange} />
-          <InputDiabetes label='Insulin' attribute='Insulin' onChange={handleChange} />
-          <InputDiabetes label='BMI' attribute='BMI' onChange={handleChange} />
-        </div>
+    <div> 
+      <InputDiabetes label='Age' attribute='Age' onChange={handleChange} />
+    </div>
 
-        <div className='sm_md:flex sm_md:space-x-4'>
-          <InputDiabetes label='Age' attribute='Age' onChange={handleChange} />
-        </div>
+    <div>
+      <button onClick={handleSubmit} className='text-white text-2xl font-semibold mt-4 mb-4'>Get Prediction</button>
+    </div>
 
-        <div className='text-left sm_md:text-left'>
-          <button onClick={handleSubmit} className='text-lg my-6 w-48 border-2 border-black rounded-md font-semibold hover:border-blue-400'>Diabetes Test Results</button>
-          
-          {isNaN(prediction) ? (
-            <p></p>
-            ) : (
-              <span className='text-center ml-4'>{prediction}</span>
-            )}
-        </div>
-
-
-      </div>
+    <div>
+      <span className='text-white'>{prediction}</span>
     </div>
 
 
+   </div>
+
+
+    
+    </div>
 
     </div>
   );
 };
 
-export default SimpleComponent;
+export default Diabetes;
