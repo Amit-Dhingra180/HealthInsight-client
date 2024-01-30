04c2da0ba1eb552e39a1da8e9b23d5466762215c
Amit Dhingra <amitdhingra180@gmail.com>
2024-01-30 21:04:23 +0530

    final

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+          <div className="bg-cover bg-top min-h-screen " style={{ backgroundImage: 'url(/doctors2.jpg)' }}>
+            <Link to='/diabetes'>
+              Diabetes
+            </Link>
+          </div>
+
+    </div>
   )
 }
 
